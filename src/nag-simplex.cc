f3d2c5812b1f43e3359feace87db6cb39d2a5ec0
Benjamin Chrétien <chretien.b@gmail.com>
2016-02-03 22:49:58 +0900

    nag-simplex.cc: reformat

--- src/nag-simplex.cc
+++ src/nag-simplex.cc
@@ -25,12 +25,12 @@
 
 #include <roboptim/core/plugin/nag/nag-simplex.hh>
 
-#define DEFINE_PARAMETER(KEY, DESCRIPTION, VALUE)	\
-  do {							\
-    this->parameters_[KEY].description = DESCRIPTION;	\
-    this->parameters_[KEY].value = VALUE;		\
+#define DEFINE_PARAMETER(KEY, DESCRIPTION, VALUE)     \
+  do                                                  \
+  {                                                   \
+    this->parameters_[KEY].description = DESCRIPTION; \
+    this->parameters_[KEY].value = VALUE;             \
   } while (0)
-
 
 namespace roboptim
 {
@@ -38,49 +38,44 @@ namespace roboptim
   {
     namespace detail
     {
-      static void
-      solverCallback (Integer ROBOPTIM_DEBUG_ONLY (n),
-		      const double xc[],
-		      double* fc,
-		      Nag_Comm* comm)
+      static void solverCallback (Integer ROBOPTIM_DEBUG_ONLY (n),
+                                  const double xc[], double* fc, Nag_Comm* comm)
       {
-	assert (!!comm);
-	assert (!!comm->p);
-	Simplex* solver = static_cast<Simplex*> (comm->p);
-	assert (!!solver);
-	assert (n == solver->problem ().function ().inputSize ());
+        assert (!!comm);
+        assert (!!comm->p);
+        Simplex* solver = static_cast<Simplex*> (comm->p);
+        assert (!!solver);
+        assert (n == solver->problem ().function ().inputSize ());
 
-	Eigen::Map<const Function::vector_t> x_
-	  (xc, solver->problem ().function ().inputSize ());
-	Eigen::Map<Function::vector_t> fc_
-	  (fc, solver->problem ().function ().outputSize ());
+        Eigen::Map<const Function::vector_t> x_ (
+          xc, solver->problem ().function ().inputSize ());
+        Eigen::Map<Function::vector_t> fc_ (
+          fc, solver->problem ().function ().outputSize ());
 
-	fc_.setZero ();
-	fc_ = solver->problem ().function () (x_);
+        fc_.setZero ();
+        fc_ = solver->problem ().function () (x_);
 
-	if (!solver->callback ())
-	  return;
-	solver->solverState ().x () = x_;
-	solver->callback () (solver->problem (), solver->solverState ());
+        if (!solver->callback ()) return;
+        solver->solverState ().x () = x_;
+        solver->callback () (solver->problem (), solver->solverState ());
       }
 
-      static void
-      printFunction (const Nag_Search_State* ROBOPTIM_DEBUG_ONLY (st),
-		     Nag_Comm* ROBOPTIM_DEBUG_ONLY (comm))
+      static void printFunction (
+        const Nag_Search_State* ROBOPTIM_DEBUG_ONLY (st),
+        Nag_Comm* ROBOPTIM_DEBUG_ONLY (comm))
       {
-	assert (st);
-	assert (comm);
-	//FIXME: write this.
+        assert (st);
+        assert (comm);
+        // FIXME: write this.
       }
     } // end of namespace detail
 
-
     Simplex::Simplex (const problem_t& pb)
       : parent_t (pb),
-	x_ (problem ().function ().inputSize ()),
-	f_ (problem ().function ().outputSize ()),
-	callback_ (),
-	solverState_ (pb)
+        x_ (problem ().function ().inputSize ()),
+        f_ (problem ().function ().outputSize ()),
+        callback_ (),
+        solverState_ (pb)
     {
       x_.setZero ();
       f_.setZero ();
@@ -89,33 +84,26 @@ namespace roboptim
       DEFINE_PARAMETER ("max-iterations", "number of iterations", 3000);
 
       // Custom parameters
-      DEFINE_PARAMETER
-	("nag.list",
-	 "if true, problem will be displayed by the NAG solver", true);
-      DEFINE_PARAMETER
-	("nag.print_level",
-	 "verbosity level", Nag_NoPrint);
-      DEFINE_PARAMETER
-	("nag.outfile",
-	 "output file used by the NAG logging system",
-	 std::string ("/tmp/nag.out"));
-      DEFINE_PARAMETER
-	("nag.optim_tol",
-	 "the accuracy in x  to which the solution is required",
-	 Function::epsilon ());
-
+      DEFINE_PARAMETER ("nag.list",
+                        "if true, problem will be displayed by the NAG solver",
+                        true);
+      DEFINE_PARAMETER ("nag.print_level", "verbosity level", Nag_NoPrint);
+      DEFINE_PARAMETER ("nag.outfile",
+                        "output file used by the NAG logging system",
+                        std::string ("/tmp/nag.out"));
+      DEFINE_PARAMETER ("nag.optim_tol",
+                        "the accuracy in x  to which the solution is required",
+                        Function::epsilon ());
     }
 
     Simplex::~Simplex ()
     {
     }
 
-    void
-    Simplex::solve ()
+    void Simplex::solve ()
     {
       // Solution.
-      if (problem ().startingPoint ())
-	x_ = *(problem ().startingPoint ());
+      if (problem ().startingPoint ()) x_ = *(problem ().startingPoint ());
 
       // Nag communication object.
       Nag_Comm comm;
@@ -131,29 +119,28 @@ namespace roboptim
       Nag_E04_Opt options;
       memset (&options, 0, sizeof (Nag_E04_Opt));
       nag_opt_init (&options);
-      options.list = static_cast<Nag_Boolean>
-	(boost::get<bool> (this->parameters_["nag.list"].value));
-      options.print_level = static_cast<Nag_PrintType>
-	(boost::get<int> (this->parameters_["nag.print_level"].value));
-      boost::get<std::string>
-	(this->parameters_["nag.outfile"].value).copy (options.outfile, 80);
+      options.list = static_cast<Nag_Boolean> (
+        boost::get<bool> (this->parameters_["nag.list"].value));
+      options.print_level = static_cast<Nag_PrintType> (
+        boost::get<int> (this->parameters_["nag.print_level"].value));
+      boost::get<std::string> (this->parameters_["nag.outfile"].value)
+        .copy (options.outfile, 80);
       options.print_fun = &detail::printFunction;
       options.max_iter =
-	boost::get<int> (this->parameters_["max-iterations"].value);
+        boost::get<int> (this->parameters_["max-iterations"].value);
 
-      nag_opt_simplex
-	(problem ().function ().inputSize (),
-	 &detail::solverCallback,
-	 &x_[0], &f_[0], &options, &comm, &fail);
+      nag_opt_simplex (problem ().function ().inputSize (),
+                       &detail::solverCallback, &x_[0], &f_[0], &options, &comm,
+                       &fail);
 
-    if (fail.code == NE_NOERROR)
+      if (fail.code == NE_NOERROR)
       {
-	Result res (problem ().function ().inputSize (),
-		    problem ().function ().outputSize ());
-	res.x = x_;
-	res.value = f_;
-	result_ = res;
-	return;
+        Result res (problem ().function ().inputSize (),
+                    problem ().function ().outputSize ());
+        res.x = x_;
+        res.value = f_;
+        result_ = res;
+        return;
       }
 
       this->result_ = SolverError (fail.message);
@@ -161,15 +148,13 @@ namespace roboptim
   } // end of namespace nag.
 } // end of namespace roboptim.
 
-extern "C"
-{
+extern "C" {
   typedef roboptim::nag::Simplex Simplex;
   typedef roboptim::Solver<roboptim::EigenMatrixDense> solver_t;
 
   ROBOPTIM_DLLEXPORT unsigned getSizeOfProblem ();
   ROBOPTIM_DLLEXPORT const char* getTypeIdOfConstraintsList ();
-  ROBOPTIM_DLLEXPORT solver_t* create
-  (const Simplex::problem_t& pb);
+  ROBOPTIM_DLLEXPORT solver_t* create (const Simplex::problem_t& pb);
   ROBOPTIM_DLLEXPORT void destroy (solver_t* p);
 
   ROBOPTIM_DLLEXPORT unsigned getSizeOfProblem ()
@@ -182,8 +167,7 @@ extern "C"
     return typeid (Simplex::problem_t::constraintsList_t).name ();
   }
 
-  ROBOPTIM_DLLEXPORT solver_t* create
-  (const Simplex::problem_t& pb)
+  ROBOPTIM_DLLEXPORT solver_t* create (const Simplex::problem_t& pb)
   {
     return new roboptim::nag::Simplex (pb);
   }
